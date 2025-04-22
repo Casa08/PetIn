f62958cd3eb2c6abb9f52fd769997889cde09054
Casa08 <derrickotieno93@gmail.com>
2025-04-22 15:01:59 +0200

    Created automated test for formating currency

---
+++ scripts/tests/money_test.js
@@ -0,0 +1,26 @@
+import { centsToDollars } from '../utilities/money_handling.js';
+
+console.log('test suite: format currency');
+console.log('converts cents to dollars');
+
+if (centsToDollars(1095) === '10.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('working with 0');
+
+if (centsToDollars(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+
+if (centsToDollars(5000.5) === '50.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
