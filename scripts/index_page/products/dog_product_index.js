e3de2b21db27b4858eefff0d079b99e5bca1ff42
Casa08 <163281668+Casa08@users.noreply.github.com>
2025-04-28 14:51:47 +0200

    Update dog_product_index.js

--- scripts/index_page/products/dog_product_index.js
+++ scripts/index_page/products/dog_product_index.js
@@ -11,82 +11,50 @@ class Product {
     this.id = productDetails.id;
     this.image = productDetails.image;
     this.name = productDetails.name;
-    this.rating = productDetails.rating
+    this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
   }
 
   getStarsUrl() {
-      return `../images/ratings/rating-${this.rating.stars * 10}.png`;
-    }
-  
+    return "images/ratings/rating-" + (this.rating.stars * 10) + ".png";
+  }
+
   getPrice() {
-    return `$${(centsToDollars(this.priceCents))}`;
+    return `$${centsToDollars(this.priceCents)}`;
   }
 }
 
-
-
-export const products =[
+export const products = [
   {
     id: "df3638ce-6aa0-4b85-b27f-e1d07eb678c61",
-    image: "../../../../images/products/df_1.webp",
+    image: "images/products/df_1.webp",
     name: "Amazon Brand - Wag Dry Dog Food Salmon & Sweet Potato, Grain Free 24 lb Bag",
-    rating: {
-      stars: 4.5,
-      count: 6878
-    },
+    rating: { stars: 4.5, count: 6878 },
     priceCents: 5133,
-    keywords: [
-      "amazon",
-      "salmon",
-      "potato"
-    ]
+    keywords: ["amazon", "salmon", "potato"]
   },
   {
     id: "df3638ce-6aa0-4b85-b27f-e1d07eb6782",
-    image: "../../../images/products/df_2.webp",
+    image: "images/products/df_2.webp",
     name: "Amazon Brand - Wag Adult Premium Nutrition Dry Dog Food, Supports Stomach and Skin Health, Lamb and Brown Rice, 30 lb Bag",
-    rating: {
-      stars: 4.0,
-      count: 68
-    },
+    rating: { stars: 4.0, count: 68 },
     priceCents: 6000,
-    keywords: [
-      "amazon",
-      "premium",
-      "nutrition"
-    ]
+    keywords: ["amazon", "premium", "nutrition"]
   },
   {
     id: "df3638ce-6aa0-4b85-b27f-e1d07eb678c3",
-    image: "../../../images/products/df_3.webp",
+    image: "images/products/df_3.webp",
     name: "Instinct Raw Boost Natural Dry Dog Food with Freeze Dried Pieces, Grain Free, Real Chicken - Gut Health, 18 lb. Bag",
-    rating: {
-      stars: 4.5,
-      count: 415
-    },
+    rating: { stars: 4.5, count: 415 },
     priceCents: 8999,
-    keywords: [
-      "instinct",
-      "raw",
-      "natural"
-    ]
+    keywords: ["instinct", "raw", "natural"]
   },
   {
     id: "dt43638ce-6aa0-4b85-b27f-e1d07eb678c1",
-    image: "../../../images/products/dt_1.webp",
+    image: "images/products/dt_1.webp",
     name: "Benebone Wishbone Durable Dog Chew Toy for Aggressive Chewers, Real Bacon, Made in USA, Medium",
-    rating: {
-      stars: 4.5,
-      count: 100
-    },
+    rating: { stars: 4.5, count: 100 },
     priceCents: 1278,
-    keywords: [
-      "wish",
-      "bone",
-      "chew"
-    ]
+    keywords: ["wish", "bone", "chew"]
   }
-].map((productDetails) => {
-  return new Product (productDetails);
-});
+].map(productDetails => new Product(productDetails));
